1577aac515426fe609984eaf3ca99232d0b4b206
Vishal Tayade <tayadevishal00@gmail.com>
2024-10-10 00:13:19 +0530

    experience section

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,7 @@
 const line = document.getElementsByClassName('line')[0];
 const body = document.getElementsByTagName('body')[0];
 const logoEdu = document.getElementsByClassName('logo-edu')[0];
+const logoExp = document.getElementsByClassName('logo-exp')[0];
 const headerLinks = document.querySelectorAll('header a');
 
 let lineHeight = line.getAttribute('style');
@@ -9,11 +10,9 @@ window.addEventListener('scroll', () => {
     if(window.scrollY > 100){
         let h = window.scrollY + lineHeight + 500;
         line.setAttribute('style', `height:${h}px`);
-        if(h>880){
-            logoEdu.style.display = 'block';
-        }else{
-            logoEdu.style.display = 'none';
-        }
+        
+        h > 880 ? logoEdu.style.display = 'block' : logoEdu.style.display = 'none';
+        h > 1600 ? logoExp.style.display = 'block' : logoExp.style.display = 'none';
     }
 });
 
